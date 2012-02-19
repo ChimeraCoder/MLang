3da76b1372ad943657a26d5025ca5bb696dfff84
nikhil <nikhil@ubuntu.ubuntu-domain>
2012-02-19 08:31:47 -0500

    Added Mexp definition
    Signed-off-by: nikhil <nikhil@ubuntu.ubuntu-domain>

---
+++ Mexp.ml
@@ -0,0 +1,24 @@
+type ('a, 'b) cell = { mutable car: 'a; mutable cdr: 'b }
+
+type t = Atom of string
+  | Cons of (t, t) cell
+  | Func of (t -> (string, t) Hashtbl.t -> t)
+  | Lambda of t * t
+  | Null
+             
+let car o =
+  match o with
+      Cons (c) -> c.car
+  | _ -> bad_arg "bad argument"
+
+let cdr o =
+  match o with
+      Cons (c) -> c.cdr
+  | _ -> bad_arg "bad argument"
+
+let cons first second = Cons { car = first ; cdr = second }
+  
+let name o =
+  match o with
+      Atom (s) -> s
+  | _ -> bad_arg "bad argument"
