561ef1e761509cd20d87f11beb2137de6c971cb5
Nikhil Sarda <nikhil@ubuntu.ubuntu-domain>
2012-04-10 22:12:30 -0400

    Added some helper functions.
    Signed-off-by: Nikhil Sarda <nikhil@ubuntu.ubuntu-domain>

--- midge.ml
+++ midge.ml
@@ -2,5 +2,11 @@ module Midge =
   struct 
     type header = (int * int * int)
     type channel = (int * string list)
-	      and body = channel list          
+	      and body = channel list
+
+    let add_channel channel body =
+      List.append body [channel]
+
+    let add_note note channel =
+      (fst channel, List.append (snd channel) [note])
   end;;
