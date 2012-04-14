fff4caa5b244928724e95095bb00f5523e4dbdf3
Nikhil Sarda <nikhil@ubuntu.ubuntu-domain>
2012-04-14 19:31:03 -0400

    Midge output complete. Given a data structure we can output it to a file.
    Example:
    
    "a";"b"])];;
    - : unit = ()
    
    This produces a soundtrack that plays the scale of C major, 3rd
    octave using a bass guitar.
    
    Signed-off-by: Nikhil Sarda <nikhil@ubuntu.ubuntu-domain>

--- midge.ml
+++ midge.ml
@@ -1,3 +1,5 @@
+open Printf
+
 module Midge = 
   struct 
     type header = (int * int * int)
@@ -9,4 +11,17 @@ module Midge =
 
     let add_note note channel =
       (fst channel, List.append (snd channel) [note])
+
+    let print_midge file (temp, num, den) body =
+      let count = ref 0 in
+      let oc = open_out file in
+        fprintf oc "@head { \n $time_sig %d/%d \n $tempo %d \n }" num den temp;
+        fprintf oc "\n @body { \n";
+        List.iter (fun (patch, notes) ->
+	  count := !count + 1;
+	  fprintf oc " @channel %d { \n $patch %d \n" !count patch;
+	  List.iter (fun note -> fprintf oc " /l4/%s3 " note) notes;
+	  fprintf oc " \n }") body;
+        fprintf oc "\n }";
+      close_out oc
   end;;
